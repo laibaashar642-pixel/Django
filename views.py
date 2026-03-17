279ba3b5c4014a0bba10f1894410d9ba89e3b03a
Laiba Ashar <asharmunir642@gmail.com>
2026-03-17 07:15:33 +0500

    Modify index view to include more context variables
    Updated the index view to send additional context variables to the template.

--- views.py
+++ views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
-    context={
+'''    context={
        "variables":"this is sent",
     }
-    return render(request,'index.html')
+    return render(request,'index.html',context)'''
+      context={
+     "var1":"harry is great",
+     "var2":"Laiba is great",
+  }
+  return render(request,'index.html',context)
     #return HttpResponse("Hello,World!")
 def about(request):
     return HttpResponse("This is the about page!")
