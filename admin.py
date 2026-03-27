675583401f60c7ac3f38a225079f104255d7a539
Laiba Ashar <asharmunir642@gmail.com>
2026-03-27 10:18:04 +0500

    Refactor admin.py to improve comments and structure
    Updated comments and added Contact model registration.

--- admin.py
+++ admin.py
@@ -1,8 +1,19 @@
-from django.contrib import admin
 
-from django.contrib import admin
 
 # Register your models here.
 #admin ko hum access krny kai liya https wali request hai us kai sth admin lga lai gai aur wo hamay admin page pr le jye ab hamnay jo aik superuser create krna hai tu wo hum aesy kry gai kai python manage.py makemigrations likhy gai phir python manage.py migrate phir python manage.py createsuperuser aur phir hamary pass admin wagera  ka name aye gha email password admin123 hai
 
 # Register your models here.
+""" from django.contrib import admin
+
+# Register your models here.
+#admin ko hum access krny kai liya https wali request hai us kai sth admin lga lai gai aur wo hamay admin page pr le jye ab hamnay jo aik superuser create krna hai tu wo hum aesy kry gai kai python manage.py makemigrations likhy gai phir python manage.py migrate phir python manage.py createsuperuser aur phir hamary pass admin wagera  ka name aye gha email password admin123 hai
+from home.models import Contact
+admin.site.register(Contact) make migration ka matlb make change and store in  a file aur migrate ka mtlb hai apply the pending changes created by makemigrations """
+from django.contrib import admin
+from .models import Contact
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "phone", "desc", "date")
